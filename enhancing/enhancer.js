0deefba936bf886cb16b14c55eecf9dba123c436
Jeremy Brady <rhspunkerkid05@yahoo.com>
2019-06-17 12:05:17 -0700

    repair and fail written and tested

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -10,7 +10,29 @@ function succeed(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const failedItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+    return failedItem;
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    const medEnhItemFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement
+    };
+    return medEnhItemFail;
+  } else if (item.enhancement > 16) {
+    const highEnhItemFail = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1
+    };
+    return highEnhItemFail;
+  }
+  return item;
 }
 
 function repair(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -5,16 +5,60 @@ describe('enhancer.js', () => {
         it('Reset an item durability to 100', () => {
             const item = {
                 name: 'Sword',
-                durability: '35',
+                durability: 35,
                 enhancement: 10
             };
             const expected = {
                 name: 'Sword',
-                durability: '100',
+                durability: 100,
                 enhancement: 10
             };
             const repairedItem = repair(item);
             expect(repairedItem).toEqual(expected);
         });
     });
+    describe('fail()', () => {
+        it('Drops durability by 5 if enhancement is less than 15', () => {
+            const item = {
+                name: 'Sword',
+                durability: 50,
+                enhancement: 14
+            };
+            const expected = {
+                name: 'Sword',
+                durability: 45,
+                enhancement: 14
+            };
+            const failedItem = fail(item);
+            expect(failedItem).toEqual(expected);
+        });
+        it('Drops durability by 10 if enhancement === 15, 16', () => {
+            const item = {
+                name: 'Sword',
+                durability: 50,
+                enhancement: 16
+            };
+            const expected = {
+                name: 'Sword',
+                durability: 40,
+                enhancement: 16
+            };
+            const failedItem = fail(item);
+            expect(failedItem).toEqual(expected);
+        });
+        it('Drops durability by 10 and enhancement by 1 if enhancement > 16', () => {
+            const item = {
+                name: 'Sword',
+                durability: 50,
+                enhancement: 18
+            };
+            const expected = {
+                name: 'Sword',
+                durability: 40,
+                enhancement: 17
+            };
+            const failedItem = fail(item);
+            expect(failedItem).toEqual(expected);
+        });
+    })
 })
